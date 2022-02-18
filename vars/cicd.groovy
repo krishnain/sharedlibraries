d68013486e5f7a7c340719f33589b29483f10749
krishnain <86010658+krishnain@users.noreply.github.com>
2022-02-18 20:03:50 +0530

    b

---
+++ vars/cicd.groovy
@@ -0,0 +1,21 @@
+def newGit(repo)
+{
+  git "${repo}" 
+}
+def newMaven()
+{
+   sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/DeclarativePipeline/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(path)
+{
+  sh "java -jar ${path}/testing.jar"
+}
+
+
+
+
+
